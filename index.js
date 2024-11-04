c087d24ae6c4a484bdcdc73ce760a22bdc7b944c
Arzeno <arzenoeugenio@gmail.com>
2024-11-03 21:08:16 -0300

    saco views

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 require("dotenv").config();
 
 const app = express();
-app.set("views", path.join(__dirname, "views"));
+
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
